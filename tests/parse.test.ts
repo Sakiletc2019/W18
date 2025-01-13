fd02b720d36ec2c37954970d5053c0be05e96bfa
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-01-13 16:10:26 +0530

    testing code for parser.ts

--- tests/parse.test.ts
+++ tests/parse.test.ts
@@ -1,8 +1,8 @@
 // tests/parser.test.ts
-import { TypeScriptParser } from '../src/engine/parser.ts';
+import { TypeScriptParser } from '../src/engine/parser';
 
 Deno.test("Test TypeScript Parser", () => {
   const tsCode = "let message: string = 'Hello, World!';";
-  const parsedCode = TypeScriptParser.parse(tsCode);
-  console.assert(parsedCode === tsCode, "Parser output should match input code.");
+  const parsedAST = TypeScriptParser.parse(tsCode);
+  console.assert(parsedAST.body.length === 1, "Parser should generate 1 statement.");
 });
