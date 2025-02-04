e463ae46631ee35bae4e33c013ab38ed979752fa
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-02-04 13:03:43 +0530

    remove all the old file for add writing better optimized code

--- src/core/for.ts
+++
@@ -1,4 +0,0 @@
-for (let i:number = 0; i <= 100; i++) {
-    console.log(i);
-  }
-  
--- tests/parse.test.ts
+++
@@ -1,8 +0,0 @@
-// tests/parser.test.ts
-import { TypeScriptParser } from '../src/engine/parser.ts';
-
-Deno.test("Test TypeScript Parser", () => {
-  const tsCode = "let message: string = 'Hello, World!';";
-  const parsedAST = TypeScriptParser.parse(tsCode);
-  console.assert(parsedAST.body.length === 1, "Parser should generate 1 statement.");
-});
