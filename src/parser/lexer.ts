e9e4ccf1b32e9b64b75af2c1aba7bd8c984e64fe
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-02-05 12:25:59 +0530

    regex is incomplet in lexer.ts

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -1,1 +1,15 @@
 // tokenizer for ts code
+
+export interface Token {
+    type: string;
+    value: string;
+}
+
+export function tokenizer(source: string): Token[]{
+    const tokens: Token[]=[];
+
+    // simple ragEx based approach
+    const tokenPatterns: [RegExp, string[]]=[
+        [/^\s+/, "WHITESPACE"],
+
+}
