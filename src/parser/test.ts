e4efbd96e98fab979cdbf6f2ca46892c6a8597f4
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-02-06 10:00:20 +0530

    chore: update .gitignore, add deno.lock, and create initial test file for parser

---
+++ src/parser/test.ts
@@ -0,0 +1,8 @@
+import { ASTNode } from "./ast.ts";
+import { tokenize, Token as _Token } from "./lexer.ts";
+import { parse } from "./parser.ts";
+
+const source = `let x:string = "hello";`;
+
+console.log("Source Code: ", source);
+console.log("Tokens: ", tokenize(source));
