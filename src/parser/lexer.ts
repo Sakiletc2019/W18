2a719ad84c3f0805e7d7f1b57a0ffeb811233fc2
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-02-06 09:06:13 +0530

    enhance tokenizer function with comprehensive regex patterns and improve error handling

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -4,12 +4,49 @@ export interface Token {
     type: string;
     value: string;
 }
-
-export function tokenizer(source: string): Token[]{
+// create a token array for store ideentified tokens
+export function tokenize(source: string): Token[]{
     const tokens: Token[]=[];
 
-    // simple ragEx based approach
-    const tokenPatterns: [RegExp, string[]]=[
-        [/^\s+/, "WHITESPACE"],
+    // simple ragEx based approach for tokenizing the input string or source code
+    const tokenPatterns: [RegExp, string][]=[
+        [/^\s+/, "WHITESPACE"],                         // Matches whitespace (space,tab,newline)
+        [/^let|const|var/, "KEYWORD"],                  // Matches keywords let, const, var
+        [/^[a-zA-Z_][a-zA-Z0-9_]*/, "IDENTIFIER"],      // Matches Variable names (identifier)
+        [/^:/,"COLON"],                                 // Matches colon character (:)
+        [/^string|number|boolean/,"TYPE"],              // Matches type keywords string, number, boolean
+        [/^=/,"ASSIGN"],                                // Matches assignment operator (=)                              
+        [/^"[^"]*"|'[^']*'/,"STRING"],                  // Matches single-quoted (') & doucle-quoted(") string literals
+        [/^\d+(\.\d+)?/,"NUMBER"],                      // Matches integer and floating-point number literals
+        [/^true|TRUE|false|FALSE/,"Boolean"],           // Matches boolean literals
+        [/^;/,"SEMICOLON"],                             // Matches semicolon character (;)
+    ];
 
-}
+    //loop the source code untill all the characters are tokenized
+    while(source.length>0){
+        let tokenFound = false; //flag to check if a token is found
+        let matched = false; // define matched variable
+
+        //try to match each token to the token list
+        for (let i = 0; i < tokenPatterns.length; i++) {
+            const [pattern, type] = tokenPatterns[i];
+            const matches = source.match(pattern);
+            if (matches) {
+                tokenFound = true;  //if token found, set the flag to true                       
+                if (type !== "WHITESPACE") {
+                    tokens.push({ type, value: matches[0] });
+                };
+
+                source = source.slice(matches[0].length);
+                matched = true;
+                break;  //stop checking other patterns once they found 
+            }
+        }
+
+        if(!matched || !tokenFound){
+            throw new Error("Unexpected character in input: "+source);
+        }
+
+    }
+    return tokens;
+};
