586c9d22cb7a7102bccdc55b2202031e50f6b6d0
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-02-16 22:05:02 +0530

    feat: add AST node types and interfaces for program structure

---
+++ V2/ast.ts
@@ -0,0 +1,71 @@
+export type NodeType = 
+| "VariableDeclaration"
+| "FunctionDeclaration"
+| "Assignment"
+| "BinaryExpression"
+| "UnaryExpression"
+| "Identifier"
+| "Literal"
+| "BlockStatement"
+| "Program";
+
+export interface statement{
+    type: NodeType;
+};
+
+export interface Program extends statement{
+    kind: "Program";
+    body: statement[];
+}
+
+export interface Expration extends statement{
+    kind: "Expration";
+    expression: statement;
+}
+
+export interface VariableDeclaration extends statement{
+    kind: "VariableDeclaration";
+    id: Identifier;
+    init: statement;
+}
+
+export interface FunctionDeclaration extends statement{
+    kind: "FunctionDeclaration";
+    id: Identifier;
+    params: Identifier[];
+    body: statement;
+}
+
+export interface Assignment extends statement{
+    kind: "Assignment";
+    id: Identifier;
+    init: statement;
+}
+
+export interface BinaryExpression extends statement{
+    kind: "BinaryExpression";
+    operator: string;
+    left: statement;
+    right: statement;
+}
+
+export interface UnaryExpression extends statement{
+    kind: "UnaryExpression";
+    operator: string;
+    argument: statement;
+}
+
+export interface Identifier extends statement{
+    kind: "Identifier";
+    name: string;
+}
+
+export interface Literal extends statement{
+    kind: "Literal";
+    value: string;
+}
+
+export interface BlockStatement extends statement{
+    kind: "BlockStatement";
+    body: statement[];
+}
